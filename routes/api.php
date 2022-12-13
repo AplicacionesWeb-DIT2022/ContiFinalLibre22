fa6e9821d4cb600e5369299a67c3e0d237612dc7
ContiMartin <tincho_conty_32@hotmai.com>
2022-12-13 18:51:26 -0300

    Agregado de API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Api\ProductosController;
+use App\Http\Controllers\Api\LugaresController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/producto', [ProductosController::class, 'index']);
+Route::get('/lugare', [LugaresController::class, 'index']);
