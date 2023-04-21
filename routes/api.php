034c8e9dad8b91c229057041e593fddb0dccd1d2
ContiMartin <tincho_conty_32@hotmai.com>
2023-04-21 09:48:30 -0300

    api cliente

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,9 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Api\ProductosController;
 use App\Http\Controllers\Api\LugaresController;
+use App\Http\Controllers\Api\ClientesController;
 
 Route::get('/producto', [ProductosController::class, 'index']);
-Route::get('/cliente', [ProductosController::class, 'index']);
+Route::get('/cliente', [ClientesController::class, 'index']);
 
 Route::get('/lugare', [LugaresController::class, 'index']);
